7a5253cdd8163d4749c7693fd15399ffeaa4a126
Hikkof <108770694+Hikkof@users.noreply.github.com>
2023-11-11 18:13:20 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,92 @@
+import pygame
+import sys
+from snake import Snake
+
+
+MIDNIGHT_BLUE = (25, 25, 112)
+ROYAL_BLUE = (65, 105, 225)
+
+
+class Game(object):
+    def __init__(self, ai=False):
+        self.max_fps = 10  # 60
+        self.width, self.height = 500, 500
+
+        pygame.init()
+        pygame.display.set_caption('Snake')
+
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+        self.fps_clock = pygame.time.Clock()
+        self.fps_delta = 0
+
+        self.player = Snake(self)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                    sys.exit(0)
+
+            self.fps_delta += self.fps_clock.tick()
+            while self.fps_delta > 1000.0 / self.max_fps:
+                self.tick()
+                self.fps_delta -= 1000.0 / self.max_fps
+
+                self.screen.fill((0, 0, 0))
+
+                pygame.draw.rect(self.screen, MIDNIGHT_BLUE, [0, 0, self.width, self.width / 40])
+                pygame.draw.rect(self.screen, MIDNIGHT_BLUE, [0, self.height - self.width / 40, self.width, self.height])
+                pygame.draw.rect(self.screen, MIDNIGHT_BLUE, [0, 0, self.width / 40, self.height])
+                pygame.draw.rect(self.screen, MIDNIGHT_BLUE, [self.width - self.width / 40, 0, self.width, self.height])
+
+                self.draw()
+                pygame.display.flip()
+
+    def tick(self):
+        self.player.tick()
+
+    def draw(self):
+        self.player.draw()
+
+    def game_over(self):
+        f = open("score.txt", "r")
+        best = int(f.read())
+        my_font = pygame.font.SysFont('times new roman', 15)
+
+        if best > self.player.size:
+            game_over_surface = my_font.render(f'Your score this time was : {str(self.player.size)}. '
+                                               f'Best score : {best}', True, ROYAL_BLUE)
+        if best == self.player.size:
+            game_over_surface = my_font.render(f'You achieved the previous record of : {str(self.player.size)}', True,
+                                               ROYAL_BLUE)
+        if best < self.player.size:
+            game_over_surface = my_font.render(f'You bet the previous best with the score of: {str(self.player.size)}. '
+                                               f'Previous best : {best}', True, ROYAL_BLUE)
+            f = open("score.txt", "w")
+            f.write(str(self.player.size))
+            f.close()
+
+        game_over_rect = game_over_surface.get_rect()
+        game_over_rect.midtop = (self.width / 2, self.height / 4)
+        self.screen.blit(game_over_surface, game_over_rect)
+
+        restart_surface = my_font.render('Do you want to try again? (y/n)', True, ROYAL_BLUE)
+        restart_rect = restart_surface.get_rect()
+        restart_rect.midbottom = (self.width / 2, self.height / 3)
+        self.screen.blit(restart_surface, restart_rect)
+
+        pygame.display.flip()
+
+        pygame.event.clear()
+        while True:
+            event = pygame.event.wait()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
+                Game()
+            if event.type == pygame.QUIT \
+                    or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) \
+                    or (event.type == pygame.KEYDOWN and event.key == pygame.K_n):
+                sys.exit(0)
+
+
+if __name__ == "__main__":
+    Game()
---
+++ snake.py
@@ -0,0 +1,68 @@
+import pygame
+import random
+
+
+RED = (255, 0, 50)
+GREEN = (0, 128, 0)
+
+
+class Snake(object):  # sterowanie przy pomocy w, a, s, d
+    def __init__(self, game):
+        self.game = game
+        self.size = 1
+
+        self.pos = pygame.math.Vector2(self.game.width*0.475, self.game.height*0.475)
+        self.vel = pygame.math.Vector2(0, -self.game.height/20)
+        self.body = [self.pos]
+        self.fruit_pos = self.fruit()
+
+    def tick(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_w] and self.vel != pygame.math.Vector2(0, self.game.height/20):
+            self.vel = pygame.math.Vector2(0, -self.game.height/20)
+        if pressed[pygame.K_s] and self.vel != pygame.math.Vector2(0, -self.game.height/20):
+            self.vel = pygame.math.Vector2(0, self.game.height/20)
+        if pressed[pygame.K_d] and self.vel != pygame.math.Vector2(-self.game.width/20, 0):
+            self.vel = pygame.math.Vector2(self.game.width/20, 0)
+        if pressed[pygame.K_a] and self.vel != pygame.math.Vector2(self.game.width/20, 0):
+            self.vel = pygame.math.Vector2(-self.game.width/20, 0)
+        last_x, last_y = self.body[-1].x, self.body[-1].y
+        for i in range(len(self.body)-1, 0, -1):
+            self.body[i].x, self.body[i].y = self.body[i-1].x, self.body[i-1].y
+        self.pos += self.vel
+
+        self.collision()
+        self.eat(last_x, last_y)
+
+    def collision(self):
+        if self.pos.x < 0 or self.pos.x+self.game.width/20 > self.game.width:
+            self.game.game_over()
+        if self.pos.y < 0 or self.pos.y+self.game.height/20 > self.game.height:
+            self.game.game_over()
+
+        for i in self.body[1:]:
+            if self.pos.x == i.x and self.pos.y == i.y:
+                self.game.game_over()
+
+    def draw(self):
+        for i in self.body:
+            rect = pygame.Rect(i.x, i.y, self.game.width / 20, self.game.height / 20)
+            pygame.draw.rect(self.game.screen, GREEN, rect)
+        rect = pygame.Rect(self.fruit_pos.x, self.fruit_pos.y, self.game.width / 20, self.game.height / 20)
+        pygame.draw.rect(self.game.screen, RED, rect)
+
+    def eat(self, last_x, last_y):
+        if self.pos.x == self.fruit_pos.x and self.pos.y == self.fruit_pos.y:
+            self.size += 1
+            self.body.append(pygame.math.Vector2(last_x, last_y))
+            self.fruit_pos = pygame.math.Vector2(self.fruit())
+
+    def fruit(self):
+        fruit = pygame.math.Vector2(random.randrange(0, self.game.width - self.game.width/20, self.game.width/20) + self.game.width/40,
+                                    random.randrange(0, self.game.height - self.game.height/20, self.game.height/20) + self.game.width/40)
+        while fruit in self.body:
+            fruit = pygame.math.Vector2(random.randrange(0, self.game.width - self.game.width / 20,
+                                                         self.game.width / 20) + self.game.width / 40,
+                                        random.randrange(0, self.game.height - self.game.height / 20,
+                                                         self.game.height / 20) + self.game.width / 40)
+        return fruit
